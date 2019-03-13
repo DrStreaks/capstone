be8117756e10cbc2884b1df890072591265a7161
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2019-03-12 21:17:19 -0400

    PHP Global Header and Footer Files

---
+++ includes/footer.php
@@ -0,0 +1,67 @@
+<footer id="index_footer">
+  <div class="footer_info">
+    <ul>
+      <li><h4>Information</h4></li>
+      <li><a>Accessibility</a></li>
+      <li><a>Emergency Information</a></li>
+      <li><a>For Our Alumni</a></li>
+      <li><a>For the Media</a></li>
+      <li><a>Jobs & Employment</a></li>
+      <li><a>Life at KSU</a></li>
+      <li><a>Privacy Statement</a></li>
+      <li><a>Tuition</a></li>
+      <li><a>Technology Support</a></li>
+      <li><a>Website Feedback</a></li>
+    </ul>
+  </div>
+  <div class="footer_camp_res">
+    <ul>
+    <li><h4>Campus Resources</h4></li>
+    <li><a>Annual Security Reports</a></li>
+    <li><a>Campus Safety</a></li>
+    <li><a>Commuter Students</a></li>
+    <li><a>Health Services</a></li>
+    <li><a>Off-Campus Students</a></li>
+    <li><a>Student Services</a></li>
+    <li><a>Transportation</a></li>
+    </ul>
+  </div>
+  <div class="footer_mail_address">
+    <ul>
+      <li><h4>Mailing Address</h4></li>
+      <li><a>P.O. Box 5190</a></li>
+      <li><a>Kent, OH 44242-0001</a></li>
+    </ul>
+  </div>
+  <div class="footer_str_address">
+    <ul>
+      <li><h4>Street Address</h4></li>
+      <li><a>800 E. Summit St.</a></li>
+      <li><a>Kent, OH 44242</a></li>
+    </ul>
+  </div>
+  <div class="footer_contact">
+    <ul>
+      <li><h4>Contact Us</h4></li>
+      <li><a>330-672-3000</a></li>
+      <li><a>info@kent.edu</a></li>
+    </ul>
+  </div>
+  <div class="footer_social_media">
+    <ul class="social_media_container">
+      <li><a><i class="fab fa-facebook-square"></i></a></li>
+      <li><a><i class="fab fa-twitter-square"></i></a></li>
+      <li><a><i class="fab fa-youtube"></i></a></li>
+      <li><a><i class="fab fa-instagram"></i></a></li>
+      <li><a><i class="fab fa-linkedin"></i></a></li>
+      <li><a><i class="fab fa-snapchat-square"></i></a></li>
+      <li><a><i class="fab fa-pinterest-square"></i></a></li>
+      <li><a><i class="fas fa-ellipsis-h"></i></a></li>
+    </ul>
+  </div>
+  <div class="footer_logo_container">
+  <img class="index_logo" src="Images\kent_state Horizontal_White.png" alt="">
+  </div>
+</footer>
+</body>
+</html>
