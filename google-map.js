6b4d6f2d3059b220c5345224f642d45664ecdcf3
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-03-28 18:18:12 -0400

    Information window (Working)
    The info window is working along with buttons

--- google-map.js
+++ google-map.js
@@ -24,66 +24,56 @@ var kentStateParkingMap;
 kentStateParkingMap = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 /*--------------- Test Code -------------------- */
-// Use the LatLng object to store the position of the marker using the object constructor
-// syntax. We will call this object pinLocation.
+// Use the latitude and longitude arrays that store the position of the marker
+// along with the name and button number using the object constructor
 var parkingLots = [
-  {
-    pos: new google.maps.LatLng(41.1490272,-81.3488268),
-    title: "R2 Business Administration"
-  },
-  {
-    pos: new google.maps.LatLng(41.1529386,-81.3494021),
-    title: "R1 Rockwell Hall"
-  },
-  {
-    pos: new google.maps.LatLng(41.1526778,-81.3468987),
-    title: "R3 Terrace Hall"
-  },
-  {
-    pos: new google.maps.LatLng(41.1482647,-81.3423212),
-    title: "R6 Fletcher Hall"
-  },
-  {
-    pos: new google.maps.LatLng(41.1529188,-81.342836),
-    title: "Center for the Performing Arts"
-  }
+  [ "R2 Business Administration", 41.1490272,-81.3488268, 1],
+  [ "R1 Rockwell Hall", 41.1529386,-81.3494021, 2],
+  [ "R3 Terrace Hall", 41.1526778,-81.3468987,3],
+  [ "R6 Fletcher Hall", 41.1482647,-81.3423212,4],
+  [ "Center for the Performing Arts", 41.1529188,-81.342836, 5]
 ];
 
-for (var i = 0; i < parkingLots.length; i++) {
+// Implement New infowindow
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+
+for (i = 0; i < parkingLots.length; i++) {
 // The google.maps.Marker() constructor creates a marker object. It takes one parameter:
 // an object that contains literal notation.
 var marker = new google.maps.Marker({
-  // Position is the object storing the markers location (pinLocation)
-  position: parkingLots[i].pos,
+  // Position is the array storing the markers location (parkingLots)
+  position: new google.maps.LatLng(parkingLots[i][1], parkingLots[i][2]),
   // Map is the map that the marker should be added to.
   map: kentStateParkingMap,
   // Icon is the path to the image of the icon that will be displayed
   icon: { url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" },
-  title: parkingLots[i].title,
+  title: parkingLots[i][0],
   animation: google.maps.Animation.DROP
   });
 
-  ////////////////////////////////////////////////////////////////////////////////
-  var infowindow = new google.maps.InfoWindow({
-    content: marker.title
-  });
 
-  google.maps.event.addListener(marker, 'click', function() {
-    let btn = document.getElementsByClassName("parking_content")[4];
-    if (btn.style.maxHeight > "0px") {
-      //closed
-      btn.style.maxHeight = "0px";
-      infowindow.close();
-    } else {
-    // Open window
-    infowindow.open(kentStateParkingMap, marker);
-    // Open content button
-    btn.style.maxHeight = "300px";
-  }
-  });
-  ////////////////////////////////////////////////////////////////////////////////
+  // IIFE or Immediately Invoked Function Expression is a function that is fired
+  // once the interpreter comes across it.
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          var information = parkingLots[i][0] + '<br />' + "Kent, OH, 44243";
+          infowindow.setContent(information);
+          infowindow.open(kentStateParkingMap, marker);
+          let btn = document.getElementsByClassName("parking_content")[i];
+          if (btn.style.maxHeight > "0px") {
+            //closed
+            btn.style.maxHeight = "0px";
+            infowindow.close();
+          } else {
+          // Open window
+          infowindow.open(kentStateParkingMap, marker);
+          // Open content button
+          btn.style.maxHeight = "300px";
+        }
+        }
+      })(marker, i));
 };
-
 }
 
 // Step 2. The loadScript() creates a <script> element to load the Google Maps API. When it has loaded,
@@ -100,7 +90,6 @@ function loadScript() {
 // Step 1. When the page is loaded, the onload event will call the loadScript() Function
 window.onload = loadScript;
 
-////////////////////////////////////////////////////////////////////////////////
 // This function opens and closes the location buttons
 function open_close_nav() {
   // Navigate the DOM to find the child and get the element
