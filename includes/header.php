769ca5a4401ed409670a5a6cd5c4960a61163be8
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-03-31 20:51:09 -0400

    Complete Site

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Capstone Project</title>
+    <link rel="stylesheet" href="Styles\Styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  </head>
+  <body>
+    <header id="index_header">
+        <img class="index_logo" src="Images\kent_state Horizontal_White.png" alt="">
+        <i class="fas fa-bars fa-2x" id="hamber_menu"></i>
+    </header>
---
+++ includes/json.php
@@ -0,0 +1,5 @@
+<?php
+  $meters = array("lot1" => "4", "lot2" => "8");
+
+  json_encode(meters);
+?>
