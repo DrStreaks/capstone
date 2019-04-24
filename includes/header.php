765746105fd7db26d854de144a77e5d4328d12ca
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-04-24 14:22:00 -0400

    more changes

--- includes/header.php
+++ includes/header.php
@@ -13,11 +13,36 @@
         <div id="parking_services"><a href="https://www.kent.edu/parking">Parking and Transit Services</a></div>
         <div id="menu_button">
           <ul id="menu_button_wrapper">
+            <div class="dropdown">
             <li><a href="https://login.kent.edu/"><i class="fas fa-bolt fa-lg"></i></a></li>
+            <div class="dropdown-content">
+            <p>Flashline Login</p>
+            </div>
+            </div>
+            <div class="dropdown">
             <li><a href="https://www.kent.edu/calendars"><i class="fas fa-calendar fa-lg"></i></a></li>
+            <div class="dropdown-content">
+            <p>Calendars</p>
+            </div>
+            </div>
+            <div class="dropdown">
             <li><a href="https://keys.kent.edu:44220/ePROD/bwgkphon.p_viewpeopledirectory"><i class="fas fa-phone fa-lg"></i></a></li>
+            <div class="dropdown-content">
+            <p>Phone Directory</p>
+            </div>
+            </div>
+            <div class="dropdown">
             <li><a href="https://map.concept3d.com/?id=568#!ct/5500"><i class="fas fa-map-marker fa-lg"></i></a></li>
+            <div class="dropdown-content">
+            <p>Maps & Directions</p>
+            </div>
+            </div>
+            <div class="dropdown">
             <li><a href="https://www.kent.edu/calendars"><i class="fas fa-search fa-lg"></i></a></li>
+            <div class="dropdown-content">
+            <p>Search</p>
+            </div>
+            </div>
             <li id="button_radius"><a href="https://www.kent.edu/admissions/apply">APPLY</a></li>
             <li id="button_radius"><a href="https://www.kent.edu/admissions/undergraduate/schedule-visit">VISIT</a></li>
             <li id="button_radius"><a href="https://www.kent.edu/advancement/give-online-now?r=2739">GIVE NOW</a></li>
@@ -29,25 +54,52 @@
             <div class="dropdown">
               <li><a href="https://www.kent.edu/parking/about">About</a></li>
               <div class="dropdown-content">
-              <p>Hello World!</p>
+                <ul>
+                  <li>Electric Vehicle (EV) Charging Stations</li>
+                  <li>Hours of Operation/Contact Information</li>
+                  <li>Frequently Asked Questions</li>
+                  <li>New Student Parking Video</li>
+                  <li>Motorist Assistance</li>
+                  <li>Parking Data</li>
+                  <li>University and Administrative Policy Regarding Parking</li>
+                </ul>
               </div>
             </div>
             <div class="dropdown">
             <li><a href="https://www.kent.edu/parking/student-parking">Students</a></li>
             <div class="dropdown-content">
-            <p>Hello World!</p>
+              <ul>
+                <li>College of Podiatric Medicine (CPM) Students</li>
+                <li>Commuter Permits</li>
+                <li>NEOMED Students</li>
+                <li>Residence Hall Permits</li>
+                <li>Regional Campus Student Permits</li>
+                <li>Disability Permits for Students</li>
+                <li>Summer Parking</li>
+                <li>Waitlists</li>
+                <li>Parking Rules for Academic Breaks</li>
+                <li>Alternative Parking Lots for Students</li>
+              </ul>
             </div>
             </div>
             <div class="dropdown">
             <li><a href="https://www.kent.edu/parking/facultystaff-parking">Faculty/Staff</a></li>
             <div class="dropdown-content">
-            <p>Hello World!</p>
+            <ul>
+              <li>Faculty/Staff Permits</li>
+              <li>Disability Permits for Faculty/Staff</li>
+              <li>Alternative Parking Lots for Faculty/Staff</li>
+            </ul>
             </div>
             </div>
             <div class="dropdown">
             <li><a href="https://www.kent.edu/parking/visitor-parking">Visitor Parking</a></li>
             <div class="dropdown-content">
-            <p>Hello World!</p>
+            <ul>
+              <li>Construction Permit</li>
+              <li>Vendor Permit</li>
+              <li>Other Permits</li>
+            </ul>
             </div>
             </div>
             <li><a href="https://www.kent.edu/parking/event-parking">Event Parking</a></li>
